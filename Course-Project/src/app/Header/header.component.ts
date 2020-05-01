850bb1447d08fc3334c49dfb30089bbfd1521288
Abhinav <raiabhinav1995@gmail.com>
2020-05-02 05:00:06 +0530

    Making Basic Skeleton For App
    Making Basic Skeleton For App

---
+++ Course-Project/src/app/Header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component
+(
+    {
+        templateUrl: 'header.component.html',
+        selector: 'app-header'
+    }
+)
+export class HeaderComponent
+{
+
+
+
+}
