4bd3e64e9f99e943b869e7de75bef783e108890a
Abhinav <raiabhinav1995@gmail.com>
2020-05-04 18:04:30 +0530

    Made a few changes to the app to use bootstrap
    Made a few changes to the app to use bootstrap

--- Course-Project/src/app/app.module.ts
+++ Course-Project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import bootstrap from "bootstrap";
+import * as bootstrap from "bootstrap";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Header/header.component';
